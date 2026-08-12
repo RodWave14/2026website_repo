9eb5900095f99b93a5cb63718ced6e8dcba8b73b
VitasHakiGrowAGardenxxForntie <vitateulilo101@gmail.com>
2026-08-13 10:25:34 +1200

    adding a language variable

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const translateButton = document.getElementById("translateBtn");
 const title = document.getElementById("title");
 
 const description = document.getElementById("description");
+
+let isTongan = false;
